99145c9360e86d1a8ca6d6722562cd179dc1f10f
zizozizo123km <nnacar12@gmail.com>
2026-01-03 22:17:52 +0100

    Initial commit: src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Project: my-ai-app</h1>
+      <p>Created from prompt: فيسبوك </p>
+    </div>
+  );
+}
+
+export default App;
